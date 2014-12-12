17afc84f7d31dab2c83313817640636387aa7aae
Tim Johansson <tim.alexander.johansson@gmail.com>
2014-12-13 00:47:50 +0100

    adding template

---
+++ translator.tpl.php
@@ -0,0 +1,25 @@
+<div class='comment-form'>
+    <form method=post>
+        <input type=hidden name="redirect" value="<?=$this->url->create($pageId)?>">
+        <input type=hidden name="sidId" value="<?= $id ?>">
+        <input type=hidden name="pageId" value="<?=$pageId?>">
+
+         
+        <fieldset>
+        <legend>Translate from latin to crylic</legend>
+        <p><label>Text:<br/><textarea name='text'></textarea></label></p>
+        
+        <p class=buttons>
+        
+        
+            <input type='submit' name='doCreate' value='Comment' onClick="this.form.action = ''"/>
+            <input type='reset' value='Reset'/>
+            
+        </p>
+		<?php if(isset($output)) : ?>
+        <output><?=$output?></output>
+		<?php endif; ?>
+        </fieldset>
+    </form>
+</div>
+
